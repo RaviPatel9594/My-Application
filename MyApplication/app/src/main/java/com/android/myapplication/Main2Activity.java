eb29775c1ae08bd3fb8ac6070f3b3f0079d806fb
RaviPatel9594 <54326658+RaviPatel9594@users.noreply.github.com>
2019-08-23 02:48:30 -0700

    Update Main2Activity.java

--- MyApplication/app/src/main/java/com/android/myapplication/Main2Activity.java
+++ MyApplication/app/src/main/java/com/android/myapplication/Main2Activity.java
@@ -6,26 +6,30 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+    private TextView nametextview,agetextview,branchtextview,gendertextview,sapidtextview;
+    private String name,branch,gender;
+    privatr int sapid,age;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        TextView nametextview=(TextView)findViewById(R.id.nametextview);
-        String name=getIntent().getExtras().getString("name");
+        nametextview=(TextView)findViewById(R.id.nametextview);
+        name=getIntent().getExtras().getString("name");
         nametextview.setText(name);
-        TextView agetextview=(TextView)findViewById(R.id.ageTextView);
-        int age=getIntent().getExtras().getInt("age");
+        agetextview=(TextView)findViewById(R.id.ageTextView);
+        age=getIntent().getExtras().getInt("age");
         agetextview.setText(age+"");
-        TextView branchtextview=(TextView)findViewById(R.id.branchTextView);
-        String branch=getIntent().getExtras().getString("branch");
+        branchtextview=(TextView)findViewById(R.id.branchTextView);
+        branch=getIntent().getExtras().getString("branch");
         branchtextview.setText(branch);
-        TextView gendertextview=(TextView)findViewById(R.id.genderTextVIew);
-        String gender=getIntent().getExtras().getString("gender");
+        gendertextview=(TextView)findViewById(R.id.genderTextVIew);
+        gender=getIntent().getExtras().getString("gender");
         gendertextview.setText(gender);
-        TextView sapidtextview=(TextView)findViewById(R.id.sapidTextView);
-        int sapid=Integer.valueOf(getIntent().getExtras().getInt("sapid"));
+        sapidtextview=(TextView)findViewById(R.id.sapidTextView);
+        sapid=Integer.valueOf(getIntent().getExtras().getInt("sapid"));
         sapidtextview.setText(sapid+"");
 
     }
